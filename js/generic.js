5543c1503f835772995a82273a3b2e28f981913c
mgmihailov <mgmihailov74@gmail.com>
2016-10-19 02:02:58 +0300

    Added the collection of items.

--- js/generic.js
+++ js/generic.js
@@ -1,1 +1,47 @@
 // Generic JS script
+
+var items = [
+	{
+		name: "Mug",
+		quantity: 1,
+		price: 6.70,
+		condition: ,
+		additionalInfo: "A mug with Spidermen on it.",
+		seller: "Josh"
+	}, {
+		name: "Slippers",
+		quantity: 2,
+		price: 10.00,
+		condition: ,
+		additionalInfo: "A pair of slippers with the Star Wars logo on them.",
+		seller: "Jake"
+	}, {
+		name: "Bike",
+		quantity: 1,
+		price: 275.50,
+		condition: ,
+		additionalInfo: "A 1-year old mountain bike, in good shape but with a flat tire.",
+		seller: "Maggie"
+	}, {
+		name: "Football",
+		quantity: 1,
+		price: 25.00,
+		condition: ,
+		additionalInfo: "A brand new football. Only used once, no damage.",
+		seller: "Ashley"
+	}, {
+		name: "Garden Chairs",
+		quantity: 4,
+		price: 83.20,
+		condition: ,
+		additionalInfo: "Four comfortable garden chairs, a couple of years old but still very preserved.",
+		seller: "Luke"
+	}, {
+		name: "Mouse",
+		quantity: 1,
+		price: 35.00,
+		condition: ,
+		additionalInfo: "A gaming mouse with very nice design, comfortable grip and additional buttons on the side.",
+		seller: "Mary"
+	}
+];
